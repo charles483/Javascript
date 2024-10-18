29dcc1cdba9317cb2e1e013ec3e93a4e4a8235b5
charles483 <c4708911@gmail.com>
2024-10-18 14:07:06 +0300

    initial commit

--- Chapter 1/script.js
+++ Chapter 1/script.js
@@ -1,9 +1,10 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
-// values and variables
+                 // values and variables
 console.log("charles");
 console.log(23);
+
 // storing them into variables for reusing them
 
 let firstName='Churu';
@@ -21,3 +22,16 @@ console.log(myFirstJob);
 // do not use reserved keywords
 // use meaningful names for variables
 // do not start with upper case letter
+// for constants use all upper case letters 
+let PI =3.1414;
+// make the variable names as descriptive as possible 
+let myFirstJobEver='programmer';
+let currentJob='teacher';
+*/
+                 // data types 
+
+let javascriptIsFun=true;// boolean data type 
+console.log(javascriptIsFun)
+// checking the type of a valu using type of 
+
+console.log(typeof javascriptIsFun)
