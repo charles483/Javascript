2353acfb3b7fbc0714857d0ad47648a6164e4eda
charles483 <c4708911@gmail.com>
2024-10-23 19:38:11 +0300

    initial commit

--- Chapter 1/script.js
+++ Chapter 1/script.js
@@ -30,8 +30,76 @@ let currentJob='teacher';
 */
                  // data types 
 
-let javascriptIsFun=true;// boolean data type 
-console.log(javascriptIsFun)
-// checking the type of a valu using type of 
+// let javascriptIsFun=true;// boolean data type 
+// console.log(javascriptIsFun)
+// // checking the type of a valu using type of 
 
-console.log(typeof javascriptIsFun)
+// console.log(typeof javascriptIsFun)
+// console.log(typeof 'hello')
+// console.log(typeof 1)
+
+// javascriptIsFun='YES!'
+// console.log(javascriptIsFun)
+// console.log(typeof javascriptIsFun)
+
+// let year;
+// console.log(year)
+// console.log(typeof year)
+
+// year =2013;
+// console.log(year)
+// console.log(typeof year)
+
+// console.log(typeof null)
+                        // let const  and var 
+
+// let age =20;
+// age=31;
+// //  for const varables  
+// // const birthYear=1991;
+// // birthYear=1999 // cannot be reinitialized 
+
+// // const job; // cannot be a undefined 
+// var job='programmer';
+// job='teacher';
+
+                        // basic operators
+//                         // math operators
+// const now=2037;
+// const ageCharles=now-2000;
+// const ageSarah =now-2010;
+// console.log(ageCharles,ageSarah);
+// console.log(ageCharles*2, ageSarah/2);
+// // 2**3 means 2 to the power of 3=2*2*2
+// const firstName='charles';
+// const lastName='churu';
+// console.log(firstName+" "+lastName);
+
+// //    assignment operators
+
+// let sum=10 +5;
+// console.log(sum);
+// sum+=10 ;//sum=sum+10
+// console.log(sum);
+// sum*=4;
+// console.log(sum);
+// sum++
+// console.log(sum);
+// sum--;
+// sum--;
+// console.log(sum);
+
+// //    comparison operators
+
+// console.log(ageCharles>ageSarah);//>,<,>=,<=
+// console.log(ageCharles<ageSarah);
+// console.log(ageSarah>=18);
+
+// const isFullAge=ageSarah>=18;
+// console.log(isFullAge);
+// console.log(now-1991>now-2002);
+const now=2037;
+const ageCharles=now-1991;
+const ageSarah=now-2018;
+
+console.log(now-1991>now-2018)  // from mdn docs get the operator precidence table and get the operators with high precedence and thos with lower precidence 
