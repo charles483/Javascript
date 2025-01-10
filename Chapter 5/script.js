db1505a670f1592ba9cea3c6a953edb95367cd92
charles churu <c4708911@gmail.com>
2025-01-10 11:11:19 +0300

    initial commit

--- Chapter 5/script.js
+++ Chapter 5/script.js
@@ -1,7 +1,7 @@
 'use strict';
 // console.log(document.querySelector(".message")) // first dom manipulation
 // getting the text
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
 
 // what is dom and dom manipulation
 // structured representation of html documents
@@ -9,3 +9,17 @@ console.log(document.querySelector(".message").textContent);
 // dom is document object model 
 // used to interact with HTML
 
+// document.querySelector('.message').textContent = '🥳 correct number'
+// document.querySelector('.number').textContent=13
+// document.querySelector('.score').textContent=10
+
+// console.log(document.querySelector('.guess').value);
+
+// document.querySelector('.guess').value=23;
+//handling the click of a button
+
+// event listeners
+
+document.querySelector('.check').addEventListener('click', function(){
+    console.log(document.querySelector('.guess').value);
+});
